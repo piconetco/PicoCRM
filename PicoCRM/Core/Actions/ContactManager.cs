fc7fc7c152777172c0ab10ca7b403d29d1f65c71
kakabery6@gmail.com <kakabery6@gmail.com>
2021-11-14 23:02:05 +0330

    Version 0.0.1

---
+++ PicoCRM/Core/Actions/ContactManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PicoCRM.Core.Actions.Contact
+{
+   
+    public class ContactManager
+    {
+        public static  string? ContactId = null;
+        public static  string? ContactName = null;
+
+        public class ActionCreateContact
+
+        {
+            
+
+            public ActionCreateContact(string contactid)
+            {
+                ContactId = contactid;
+                 
+                MessageBox.Show(ContactId);
+            }
+        }
+        public class ActionDeleteContact
+        {
+            public ActionDeleteContact()
+            {
+                MessageBox.Show(ContactId);
+            }
+        }
+
+        public class ActionUpdateContact
+
+        {
+            public ActionUpdateContact()
+            {
+
+            }
+        }
+    }
+    
+}
--- PicoCRM/Core/Views/Oder.xaml.cs
+++ PicoCRM/Core/Views/Oder.xaml.cs
@@ -16,132 +16,18 @@ namespace PicoCRM.Core.Views
         {
             InitializeComponent();
         }
-        public string dealid { get; set; }
-        public long contactid { get; set; }
-        public long  checkid {get;set;}
-        Contact DailyForecast { get;set; }
+        
         private void btnsubmit_Click(object sender, RoutedEventArgs e)
         {
-           
-    
-            #region AutoCreateAndAssignDealToContact
+
+
             
-            Contact ContactManager = new Contact();
-           
-            Contact DailyCheckout = new Contact();
-           
-            Handler.Send SmsProvider = new Handler.Send();
           
-            Deal DealManager = new Deal();
-   
-            Create Assoc = new Create();
-  
-            DateTime thisDate = DateTime.Now;
-
-            PersianCalendar pc = new PersianCalendar();
-
-            try
-            {
-                string PersianDateConverter = pc.GetYear(thisDate).ToString() + pc.GetMonth(thisDate).ToString() + pc.GetDayOfMonth(thisDate).ToString();
-            
-
-                string current = DateTime.Now.Year.ToString() + DateTime.Now.Month.ToString() + DateTime.Now.Day.ToString();
-            
-
-
-
-                 contactid = ContactManager.Create(cFName.Text, cLName.Text, cPhone.Text, cPhone.Text + "@gmail.com");
-      
-
-
-                long checkid = DailyCheckout.Create("Day:", PersianDateConverter, PersianDateConverter, PersianDateConverter + "@gmail.com");
-          
-
-
-                dealid = DealManager.CreateDeal(cTitle.Text, cPrice.Text, cAbout.Text, "closedwon");
-            
-
-
-
-                Assoc.AssiciateContactToDeal(checkid, long.Parse(dealid));
-              
-
-
-                Assoc.AssiciateContactToDeal(contactid, long.Parse(dealid));
-            
-
-
-
-                var ContactInfo = ContactManager.GetCustomerDetails(contactid);
-                MessageBox.Show("8");
-               
-
-                string TotalRevenue = ContactInfo.properties.total_revenue.value;
-
-
-
-
-                MessageBox.Show("9");
-                var DailyForcast = DailyCheckout.GetCustomerDetails(checkid).properties.total_revenue.value;
-                
-
-        
-
-                string Report = SmsProvider.SendReportToAdmin("09109740017", cPrice.Text, cTitle.Text, DailyForcast, dealid);
-
-
-                string Report1 = SmsProvider.SendReportToAdmin("09150089472", cPrice.Text, cTitle.Text, DailyForcast, dealid);
-
-
-                string red = SmsProvider.SendTranaction(cPhone.Text, cPrice.Text, (int.Parse(TotalRevenue) / 100 * 15).ToString(), cFName.Text, cLName.Text, dealid);
-             
-                MessageBox.Show("ثبت اطلاعات با موفقی انجام شد");
-
-
-            }
-            catch (Exception ex)
-            {
-               try
-                {
-                    MessageBox.Show(ex.Data.ToString());
-                    DealManager.DeleteDeal(dealid);
-                    
-                    dealid = DealManager.CreateDeal(cTitle.Text, cPrice.Text, cAbout.Text, "closedwon");
-
-                    Assoc.AssiciateContactToDeal(checkid, long.Parse(dealid));
-
-
-                    Assoc.AssiciateContactToDeal(contactid, long.Parse(dealid));
-
-                    var ContactInfo = ContactManager.GetCustomerDetails(contactid);
-                    MessageBox.Show("18");
-
-
-                    string TotalRevenue = ContactInfo.properties.total_revenue.value;
-                    MessageBox.Show(TotalRevenue);
-
-                    var DailyForcast = DailyCheckout.GetCustomerDetails(checkid).properties.total_revenue.value;
-
-                    string Report = SmsProvider.SendReportToAdmin("09109740017", cPrice.Text, cTitle.Text, DailyForcast, dealid);
-
-                    string Report1 = SmsProvider.SendReportToAdmin("09150089472", cPrice.Text, cTitle.Text, DailyForcast, dealid);
-
-                    string red = SmsProvider.SendTranaction(cPhone.Text, cPrice.Text, (int.Parse(TotalRevenue) / 100 * 15).ToString(), cFName.Text, cLName.Text, dealid);
-
-                    MessageBox.Show("ثبت اطلاعات با موفقیت انجام شد");
-                }
-              
-                catch( Exception exx )
-                
-                {
-                    MessageBox.Show(exx.Message);
-                   
-                }
-            }
-
-
-            #endregion
         }
+
+
+         
+        
 
         private void Progress_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
